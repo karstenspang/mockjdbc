d5958a536a0d8e4932dac3d6d8e61f5b177816ca
Karsten Spang <47601460+karstenspang@users.noreply.github.com>
2023-04-11 23:14:56 +0200

    Update wrap mechanism for new Step structure.

--- src/main/java/io/github/karstenspang/mockjdbc/Wrapper.java
+++ src/main/java/io/github/karstenspang/mockjdbc/Wrapper.java
@@ -2,14 +2,16 @@ package io.github.karstenspang.mockjdbc;
 
 /**
  * A functional that puts a wrap on something.
- * @param <T> The type of both the wrap and the wrapped,
- * for example an interface that they both implement.
+ * @param <W> The type of both the wrap and the wrapped,
+ * usually an interface that they both implement.
  */
-public interface Wrapper<T>{
+@FunctionalInterface
+public interface Wrapper<W>{
     /**
      * Wrap an object.
      * @param wrapped Object to be wrapped.
+     * @param program The program for the wrap.
      * @return the wrap of the object.
      */
-    T wrap(T wrapped);
+    W wrap(W wrapped,Program program);
 }
--- src/main/java/io/github/karstenspang/mockjdbc/WrapperStep.java
+++ src/main/java/io/github/karstenspang/mockjdbc/WrapperStep.java
@@ -4,28 +4,46 @@ import java.sql.SQLException;
 
 /**
  * {@link Step} that wraps the suplied value.
- * @param <T> The type returned
+ * @param <W> The wrapped type.
  */
-public class WrapperStep<T> implements Step<T> {
-    private Wrapper<T> wrapper;
+public class WrapperStep<W> implements Step {
+    private Wrapper<W> wrapper;
+    private Program program;
     
     /**
      * Construct the step.
-     * @param wrapper defines how to wrap
+     * @param wrapper Creates the wrap
+     * @param program The steps to pass to {@code wrapper}
      */
-    public WrapperStep(Wrapper<T> wrapper){
+    public WrapperStep(Wrapper<W> wrapper,Iterable<Step> program){
         this.wrapper=wrapper;
+        this.program=new Program(program);
     }
     
     /**
      * Call the supplier, wrap the result, and return the wrap.
+     * @param <T> The type returned by {[@code supplier} and this method. Must match <code>&lt;W&gt;</code>.
      * @param supplier Called to get the result
      * @return wrapped result from {@code supplier}
      * @throws SQLException if {@code supplier} does.
+     * @throws ClassCastException if <code>&lt;W&gt;</code> and <code>&lt;T&gt;</code> do not match.
      */
-    public T apply(SQLSupplier<? extends T> supplier)
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T apply(SQLSupplier<? extends T> supplier)
         throws SQLException
     {
-        return wrapper.wrap(supplier.get());
+        return (T)wrapper.wrap((W)supplier.get(),program);
+    }
+    
+    /**
+     * Always throws {@link ClassCastException}, as there is no way to wrap {@code void}.
+     * @param action not used
+     * @throws ClassCastException always
+     */
+    @Override
+    public void apply(SQLRunnable action)
+    {
+        throw new ClassCastException("void cannot be wrapped");
     }
 }
